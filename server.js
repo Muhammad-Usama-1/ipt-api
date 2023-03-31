89ec8aeebac9e48f43f2ac207a6ff0aca45ae1b5
Muhammad Usama <mdusama225@gmail.com>
2023-03-31 22:31:42 +0500

    added origin for chat

--- server.js
+++ server.js
@@ -36,7 +36,7 @@ const server = app.listen(port, () => {
 
 const io = new Server(server, {
   cors: {
-    origin: true,
+    origin: "https://glittery-crostata-66e711.netlify.app/",
     methods: ["GET", "POST"],
   },
 });
