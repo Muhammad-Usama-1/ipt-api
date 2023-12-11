d896cd10a0b024040b6330995fad291bdda58934
Muhammad Usama <66507968+Muhammad-Usama-1@users.noreply.github.com>
2023-12-11 14:06:28 +0500

    Update authController.js

--- controllers/authController.js
+++ controllers/authController.js
@@ -29,13 +29,13 @@ exports.signup = catchAsync(async (req, res, next) => {
   console.log("here");
 
   // In this APP because we dont have role base access we can provide req.body inside it
-  // const newUser = await User.create({
-  //   name: req.body.name,
-  //   email: req.body.email,
-  //   password: req.body.password,
-  //   passwordConfirm: req.body.passwordConfirm,
-  // });
-  const newUser = await User.create(req.body);
+  const newUser = await User.create({
+     name: req.body.name,
+     email: req.body.email,
+    password: req.body.password,
+     passwordConfirm: req.body.password,
+  });
+  // const newUser = await User.create(req.body);
   // const url = ` ${req.protocol}://${req.get("host")}/me`;
   // const url = `https://github.com/Muhammad-Usama-1/node-express-boilerplaete`;
 
